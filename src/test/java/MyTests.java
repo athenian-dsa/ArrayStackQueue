6c5e2ac9f83bcfe4a36f5a139874ab40ae9ca41a
sthompson-athenian <sthompson@athenian.org>
2024-09-04 13:11:02 -0700

    Fix ArrayQueue Test

--- src/test/java/MyTests.java
+++ src/test/java/MyTests.java
@@ -4,12 +4,12 @@ import static org.junit.jupiter.api.Assertions.*;
 
 public class MyTests {
     @Test
-    public void testArrayQueue() {
+    public void testArrayQueueBasicFunctionality() {
         Queue queue = new ArrayQueue();
         queue.enqueue(10);
         assertEquals(10, queue.peek());
         queue.enqueue(5);
-        assertEquals(5, queue.peek());
+        assertEquals(10, queue.peek());
 
         assertFalse(queue.isEmpty());
 
@@ -18,5 +18,17 @@ public class MyTests {
 
         assertEquals(5, queue.dequeue());
         assertTrue(queue.isEmpty());
+    }
+
+    @Test
+    public void testArrayQueueExpansion() {
+        Queue queue = new ArrayQueue();
+        for (int i = 0; i < 200; i++) {
+            queue.enqueue(i);
+        }
+
+        while (!queue.isEmpty()) {
+            queue.dequeue();
+        }
     }
 }
