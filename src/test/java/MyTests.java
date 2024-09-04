89e990678f1e6dd8bbf3366055ba992167d267c3
sthompson-athenian <sthompson@athenian.org>
2024-09-03 22:43:23 -0700

    Update ArrayQueue to include tests

--- src/test/java/MyTests.java
+++ src/test/java/MyTests.java
@@ -1,12 +1,22 @@
-package test.java;
-
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 public class MyTests {
     @Test
-    public void testInsertFirst() {
-        assertTrue(false);
+    public void testArrayQueue() {
+        Queue queue = new ArrayQueue();
+        queue.enqueue(10);
+        assertEquals(10, queue.peek());
+        queue.enqueue(5);
+        assertEquals(5, queue.peek());
+
+        assertFalse(queue.isEmpty());
+
+        assertEquals(10, queue.dequeue());
+        assertEquals(5, queue.peek());
+
+        assertEquals(5, queue.dequeue());
+        assertTrue(queue.isEmpty());
     }
 }
